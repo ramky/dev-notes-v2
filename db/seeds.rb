fcca881ad24f78fba023a75bc31477e122bf8625
Ram Iyer <ramky.iyer@gmail.com>
2020-09-03 00:03:17 -0400

    removing password from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 Type.find_or_create_by(title: 'Dev Notes')
 Type.find_or_create_by(title: 'Addresses')
 Type.find_or_create_by(title: 'VIM Notes')
-Type.find_or_create_by(title: 'Password')
 Type.find_or_create_by(title: 'Markdown')
 Type.find_or_create_by(title: 'Quote')
